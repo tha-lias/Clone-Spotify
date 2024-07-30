2ce7c2b470581597c96792bd89863cb1997d6729
Thalia da Silva <thalia.silva@akrkpromotora.local>
2024-07-30 18:29:36 -0300

    Feat: componentes de musicPlayer e musicPlayerVolume

---
+++ src/components/MusicPlayerVolume.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+import VolumeMute from 'vue-material-design-icons/VolumeMute.vue';
+import VolumeHigh from 'vue-material-design-icons/VolumeHigh.vue';
+
+import { useSongStore } from '../stores/song'
+import { storeToRefs } from 'pinia';
+const useSong = useSongStore()
+const { audio } = storeToRefs(useSong)
+
+let isHover = ref(false)
+let vol = ref(80)
+let volume = ref(null)
+
+<script/>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+
